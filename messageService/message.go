d551a0c1c98d5885ebf3edcb1dc6c797f091d30d
YAWAL <yavorskyyval@gmail.com>
2017-07-15 15:51:52 +0300

    functions for Message Service have been fixed

--- messageService/message.go
+++ messageService/message.go
@@ -3,12 +3,8 @@ package messageService
 import (
 	"time"
 	"encoding/json"
-	"fmt"
 	"log"
 )
-
-//server address, IP and port will fill later
-const server = "IP:port"
 
 //structure for message(should be in JSON format) which consist with header and body
 type Message struct {
@@ -26,65 +22,79 @@ type MessageHeader struct {
 	Auth string `json:"auth"`// value of Auth is "token" at the beginning (maybe use JSON web token?)
 }
 
-// body for message
+// body for Message
 type MessageBody struct {
 	Text string `json:"text"`
 	//links []link -> slice of formatted URL's
 }
 
-//func unmarshal message ([] byte JSON -> Message)
-func UnmarshalMessage(byteMessage [] byte ) Message {
-	var message Message
-	err := json.Unmarshal(byteMessage, &message)
+//func for unmarshalling message ([] byte JSON -> Message struct)
+func UnmarshalMessage(byteMessage [] byte ) (Message, error){
+	var MessageStructure Message
+	err := json.Unmarshal(byteMessage, &MessageStructure)
 	if err != nil{
-		log.Println("Can't unmarshal this array of bytes :", err)
+		log.Println("error has occured: ", err)
+		return MessageStructure, err
 	}
-	return message
+	return MessageStructure, err
 }
 
-//func for marshaling message ([] byte or Message ??? -> [] byte JSON)
-func MarshalMessage(message Message) [] byte {
-	msgJSON, err := json.Marshal(message) // marshal message to JSON
+//func for marshaling message (Message -> [] byte JSON)
+func MarshalMessage(message Message) ([] byte, error ){
+	msgJSON, err := json.Marshal(message)
 	if err != nil {
-		fmt.Println("error:", err)
+		log.Println("error has occured: ", err)
+		return nil, err
 	}
-	return msgJSON
+	return msgJSON, err
 }
 
-//func from parsing type from JSON
-func GetTypeFromHeader(message Message) string {
-	//implement this
-	return "type"
+//func for unmarshalling []byte into map[string]interface{} without knowing data's structure
+func UnmarshalAnyDataStructure(byteSlice [] byte) (map[string]interface{}, error) {
+	var dataStructure interface{}
+	err := json.Unmarshal(byteSlice, &dataStructure)
+	mapStructure := dataStructure.(map[string]interface{})
+	if err != nil{
+		log.Println("error has occured: ", err)
+		return nil, err
+	}
+	return mapStructure, err
 }
 
-//func for parsing command from JSON
-func GetCommandFromHeader(message Message) string {
-	//implement this
-	return "command"
+//function for retrieving "type" value from JSON requests
+func GetType(byteSlice [] byte) (string, error) {
+	JSONMap, err := UnmarshalAnyDataStructure(byteSlice)
+	if err != nil {
+		log.Println("error has occured: ", err)
+		return "", err
+	}
+	var value string
+	for k, v := range JSONMap {
+		//value := string(v)
+		//fmt.Println(k , v)
+		if k == "type" {
+			value := v.(string)
+
+			//v := string(v)
+			return value, err
+		}
+	}
+	return value, err
 }
 
-//function for creating massages
-//from where data about SenderId, ReceiverId should come?Maybe, from UI, when user pick-up contact from contact-list
-//func  MakeMessage(SenderId, ReceiverId int, Text string)  []byte{
-	//header := MessageHeader{, SenderId, ReceiverId, time.Now(), "token"}//creating MessageHeader
-
-	//Text = "message for Go252Lv team" // this hardcoded just for initial checking
-	//body := MessageBody{Text}//text should be read from UI and passed here
-
-	//msg := Message{header, body}
-	//msgJSON, err := json.Marshal(msg) // marshal message to JSON
-	//if err != nil {
-	//	fmt.Println("error:", err)
-	//}
-	//return msgJSON
-//}
-
-//func validate message
-
-
-
-// function for sending(using WebSocket) messages to server
-func SentMessage(message Message, addr string) bool  {
-
-	return false
+//function for retrieving "command" value from JSON requests
+func GetCommand(byteSlice [] byte) (string, error) {
+	JSONMap, err := UnmarshalAnyDataStructure(byteSlice)
+	if err != nil {
+		log.Println("error has occured: ", err)
+		return "", err
+	}
+	var value string
+	for k, v := range JSONMap {
+		if k == "command" {
+			value := v.(string)
+			return value, err
+		}
+	}
+	return value, err
 }
--- messageService/message_test.go
+++ messageService/message_test.go
@@ -14,16 +14,91 @@ var (
 	CurrentTime = time.Now
 	Auth = "token"
 	Text = "msg from user 1 to user 2"
+	byteMessage = []byte(`{"header":{"type":"messageService","command":"send message","sender":1,"receiver":2,"time":"2017-07-15T01:11:04.134765+03:00","auth":"token"},"body":{"text":"msg from user 1 to user 2"}}`)
+	byteHeader = []byte(`{"type": "message", "command": "sentMessage", "sender": 1, "receiver": 2, "time": "hereAndNow", "auth": "token"}`)
+	byteBody = []byte(`{"text": "msg from user 1 to user 2"}`)
 	)
 
 func TestMarshalMessage(t *testing.T) {
 	header := MessageHeader{Type_, Command, Sender, Receiver, CurrentTime() , Auth}
 	body := MessageBody{Text}
-
 	message := Message{header, body}
-
-	//fmt.Println("Message before marshaling", message)
-	//MarshalMessage(message)
-	fmt.Println("Message after marshaling",message)
-
+	mrsMsg, err := MarshalMessage(message)
+	if err == nil {
+		fmt.Println("TestMarshalMessage: PASS, Error-> ", err)
+		fmt.Println("Message after marshaling in []byte: \n", mrsMsg)
+		fmt.Println("Message after marshaling in string: \n",string(mrsMsg) )
+		fmt.Println("========================================================")
+	}
 }
+
+func TestUnmarshalMessage(t *testing.T) {
+	var ThisMessage Message
+	ThisMessage, err := UnmarshalMessage(byteMessage)
+	if err == nil {
+		fmt.Println("TestUnmarshalMessage: PASS, Error-> ", err)
+		fmt.Printf("Message after unmarshaling has type %T \n",ThisMessage )
+		fmt.Printf("Message after unmarshaling : %+v \n ",ThisMessage )
+		fmt.Println("========================================================")
+	}
+}
+
+func Test1UnmarshalAnyDataStructure(t *testing.T) {
+	MessageInByte, err := UnmarshalAnyDataStructure(byteMessage)
+	if err == nil {
+		fmt.Println("Test1UnmarshalAnyDataStructure: PASS, Error-> ", err)
+		fmt.Printf("Message after unmarshaling has type %T \n", MessageInByte)
+		fmt.Printf("Message after unmarshaling : %+v \n ", MessageInByte)
+		fmt.Println("========================================================")
+	}
+}
+
+func Test2UnmarshalAnyDataStructure(t *testing.T) {
+	HeaderInByte, err := UnmarshalAnyDataStructure(byteHeader)
+	if err == nil {
+		fmt.Println("Test2UnmarshalAnyDataStructure: PASS, Error-> ", err)
+		fmt.Printf("Message after unmarshaling has type %T \n", HeaderInByte)
+		fmt.Printf("Message after unmarshaling : %+v \n ", HeaderInByte)
+		fmt.Println("========================================================")
+	}
+}
+
+func Test3UnmarshalAnyDataStructure(t *testing.T) {
+	BodyInByte, err := UnmarshalAnyDataStructure(byteBody)
+	if err == nil {
+		fmt.Println("Test3UnmarshalAnyDataStructure: PASS, Error-> ", err)
+		fmt.Printf("Message after unmarshaling has type %T \n", BodyInByte)
+		fmt.Printf("Message after unmarshaling : %+v \n ", BodyInByte)
+		fmt.Println("========================================================")
+	}
+}
+
+func TestGetType(t *testing.T) {
+	Value, err := GetType(byteHeader)
+	if err == nil {
+		fmt.Println("TestGetType: PASS, Error-> ", err)
+		fmt.Printf("Type is %T \n", Value)
+		if Value == "message" {
+			fmt.Printf("Correct, Value is -> %v", Value)
+		}else {
+			fmt.Printf("Wrong, \"message\" != %v", Value)
+		}
+	}
+	fmt.Println("\n ========================================================")
+}
+
+func TestGetCommand(t *testing.T) {
+	Command, err := GetCommand(byteHeader)
+	if err == nil {
+		fmt.Println("TestGetCommand: PASS, Error-> ", err)
+		fmt.Printf("Command has type %T \n", Command)
+		if Command == "sentMessage" {
+			fmt.Printf("Correct, Value is -> %v", Command)
+		}else {
+			fmt.Printf("Wrong, \"sentMessage\" != %v", Command)
+		}
+	}
+	fmt.Println("\n ========================================================")
+}
+
+

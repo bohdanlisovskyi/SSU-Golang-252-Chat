991e6673d742e85e399d71b84791f12dd34f44a7
Bohdan <www.lbohdan@gmail.com>
2017-07-17 22:06:36 +0300

    handle some error in server core, and handle message type

--- server/core/server-core.go
+++ server/core/server-core.go
@@ -1,10 +1,10 @@
 package core
 
 import (
-	"log"
 	"strconv"
 	"net/http"
 	"github.com/gorilla/websocket"
+	"github.com/8tomat8/SSU-Golang-252-Chat/chat-log"
 	"github.com/8tomat8/SSU-Golang-252-Chat/messageService"
 )
 
@@ -25,33 +25,72 @@ func MessageHandler(w http.ResponseWriter, r *http.Request) {
 
 	conn := addNewClient(w, r)
 
-	log.Println("Add new connection", conn)
+	chatlog.Log(chatlog.Fields{}, "info", "Add new connection: ", conn)
 
 	go func() {
 
 		for {
-			_, text, _ := conn.ReadMessage()
+			messageType, text, err := conn.ReadMessage()
 
-			message := messageService.UnmarshalMessage(text)
+			chatlog.HandleError(err, "Read message error: ")
 
-			//I check token this json and get receiver_id
-
-			byteMessage := messageService.MarshalMessage(message)
-
-			writeMsg(byteMessage, strconv.Itoa(message.Header.Receiver)) //I send this text []byte to receiver
+			validateMessage(messageService.UnmarshalMessage(text), messageType)
 		}
 	}()
 }
 
-func somefunc() string{return ""} //(THIS IS THE MOCK FUNC) remove this empty func when Valery write your own func //TODO remove this
+func sendMessage(message messageService.Message, messageType int) {
+
+	byteMessage := messageService.MarshalMessage(message)
+
+	writeMsg(byteMessage, strconv.Itoa(message.Header.Receiver), messageType) //I send this text []byte to receiver
+}
+
+func validateMessage(message messageService.Message, messageType int) {
+
+	//Why i don't use case? because if working faster
+
+	if message.Header.Type_ == "msg" {
+
+		sendMessage(message, messageType)
+	}
+
+	if message.Header.Type_ == "register" {
+
+		//run register function
+	}
+
+	if message.Header.Type_ == "auth" {
+
+		//run auth function
+	}
+
+	if message.Header.Type_ == "search" {
+
+		//run search contact function
+	}
+
+	if message.Header.Type_ == "list" {
+
+		//run list contact function
+	}
+
+	if message.Header.Type_ == "change_pass" {
+
+		//run change_pass function
+	}
+
+	if message.Header.Type_ == "change_info" {
+
+		//run change_user_info function
+	}
+}
 
 func addNewClient(w http.ResponseWriter, r *http.Request) *websocket.Conn{
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 
-	if err != nil {
-		log.Println(err)
-	}
+	chatlog.HandleError(err, "Connect new user Error: ")
 
 	clients[strconv.Itoa(len(clients)+1)] = Client{conn:conn}
 
@@ -59,7 +98,7 @@ func addNewClient(w http.ResponseWriter, r *http.Request) *websocket.Conn{
 }
 
 
-func writeMsg(text []byte, receiver_id string) {
+func writeMsg(text []byte, receiver_id string, messageType int) {
 
-	clients[receiver_id].conn.WriteMessage(websocket.TextMessage, text)
+	chatlog.HandleError(clients[receiver_id].conn.WriteMessage(messageType, text), "Write message error: ")
 }

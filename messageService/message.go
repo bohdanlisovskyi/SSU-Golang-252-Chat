4a65e48cfbda494049a26862f86638afc77b4a9d
YAWAL <yavorskyyval@gmail.com>
2017-07-18 17:15:20 +0300

    changes which were discussed with Borys today morning

--- messageService/message.go
+++ messageService/message.go
@@ -1,86 +1,59 @@
 package messageService
 
 import (
-	"time"
 	"encoding/json"
 	"log"
-	"errors"
 )
 
-//structure for message which sent between users
+// Message is a structure for message which is sending between users
 type Message struct {
 	Header MessageHeader `json:"header"`
 	Body   MessageBody `json:"body"`
 }
 
-//structure of header for Message
+// MessageHeader is a structure of header for Message
 type MessageHeader struct {
-	Type_    string `json:"type"`    // this named "Type_" because "type" - keyword
-	Command  string `json:"command"` //depend on action from user (e.g. sent message, set info e.t.c.)
-	Sender   int `json:"sender"`
-	Receiver int `json:"receiver"`
-	Time     time.Time `json:"time"`
-	Auth     string `json:"auth"` // value of Auth is "token" at the beginning (maybe use JSON web token?)
+	Type_    string `json:"type"`
+	Command  string `json:"command"`
+	UserName string `json:"userName"`
+	Token    string `json:"token"`
 }
 
-//structure of body for Message
+// MessageBody is a structure of body for Message
 type MessageBody struct {
-	Text string `json:"text"`
-	//links []link -> slice of formatted URL's
+	ReceiverName string `json:"receiverName"`
+	Time         int `json:"time"` // unix time will be used http://www.unixtimestamp.com/
+	Text         string `json:"text"`
 }
 
-//function for unmarshaling message (from [] byte JSON to Message structure)
-func UnmarshalMessage(byteMessage [] byte) (Message, error) {
-	var MessageStructure Message
+// UnmarshalMessage is a function for unmarshaling message (from [] byte JSON to Message structure)
+func UnmarshalMessage(byteMessage [] byte) (*Message, error) {
+	var MessageStructure *Message
 	err := json.Unmarshal(byteMessage, &MessageStructure)
 	if err != nil {
-		log.Println("Error has occured: ", err)
+		log.Println("Error has occured: ", err) // it will be synchronized with Bohdan's logger-lib after merging initial PR's
 		return MessageStructure, err
 	}
 	return MessageStructure, err
 }
 
-//function for marshaling message (Message -> [] byte JSON)
+// MarshalMessage is a function for marshaling message (Message -> [] byte JSON)
 func MarshalMessage(message Message) ([] byte, error) {
 	msgJSON, err := json.Marshal(message)
 	if err != nil {
-		log.Println("Error has occured: ", err)
+		log.Println("Error has occured: ", err) // it will be synchronized with Bohdan's logger-lib after merging initial PR's
 		return nil, err
 	}
 	return msgJSON, err
 }
 
-//function for unmarshaling request(without info about structure) in []byte  which come from UI into map[string]interface{}
+// UnmarshalRequest is a function for unmarshaling request(without info about structure) in []byte  which come from UI into map[string]interface{}
 func UnmarshalRequest(byteRequest [] byte) (map[string]interface{}, error) {
 	var unmarshaledRequest map[string]interface{}
 	err := json.Unmarshal(byteRequest, &unmarshaledRequest)
 	if err != nil {
-		log.Println("Error has occured: ", err)
+		log.Println("Error has occured: ", err) // it will be synchronized with Bohdan's logger-lib after merging initial PR's
 		return nil, err
 	}
 	return unmarshaledRequest, err
-}
-
-//function for server to retrieving "type" value from unmarshaled request
-func GetType(unmarshaledRequest map[string]interface{}) (string, error) {
-	typeInterface, ok := unmarshaledRequest["type"]
-	if !ok {
-		err := errors.New("Can't find value for type")
-		log.Println("Error has occured: ", err)
-		return "", err
-	}
-	typeValue := typeInterface.(string)
-	return typeValue, nil
-}
-
-//function for server to retrieving "command" value from unmarshaled request
-func GetCommand(unmarshaledRequest map[string]interface{}) (string, error) {
-	valInterface, ok := unmarshaledRequest["command"]
-	if !ok {
-		err := errors.New("Can't find value for command")
-		log.Println("Error has occured: ", err)
-		return "", err
-	}
-	value := valInterface.(string)
-	return value, nil
 }
--- messageService/message_test.go
+++ messageService/message_test.go
@@ -2,26 +2,24 @@ package messageService
 
 import (
 	"testing"
-	"time"
 )
 
 var (
-	Type_         = "messageService"
-	Command       = "send message"
-	Sender        = 1
-	Receiver      = 2
-	CurrentTime   = time.Now
-	Auth          = "token"
-	Text          = "msg from user 1 to user 2"
-	byteMessage   = []byte(`{"header":{"type":"messageService","command":"send message","sender":1,"receiver":2,"time":"2017-07-15T01:11:04.134765+03:00","auth":"token"},"body":{"text":"msg from user 1 to user 2"}}`)
-	byteHeader    = []byte(`{"type": "message", "command": "sentMessage", "sender": 1, "receiver": 2, "time": "hereAndNow", "auth": "token"}`)
-	byteBody      = []byte(`{"text": "msg from user 1 to user 2"}`)
-	mappedRequest = map[string]interface{}{"key1": "wrong", "key2": 1, "type": "message", "typed": "typed", "etype": "band", "comand": "wrong", "command": "sent message"}
+	Type_        = "messageService"
+	Command      = "send message"
+	UserName     = "userOne"
+	ReceiverName = "userTwo"
+	CurrentTime  = 1500373713
+	Token        = "token"
+	Text         = "msg from user 1 to user 2"
+	byteMessage  = []byte(`{"header":{"type":"messageService","command":"send message","sender":1,"receiver":2,"time":1500373713,"auth":"token"},"body":{"text":"msg from user 1 to user 2"}}`)
+	byteHeader   = []byte(`{"type": "message", "command": "sentMessage", "sender": 1, "receiver": 2, "time": "hereAndNow", "auth": "token"}`)
+	byteBody     = []byte(`{"text": "msg from user 1 to user 2"}`)
 )
 
 func TestMarshalMessage(t *testing.T) {
-	header := MessageHeader{Type_, Command, Sender, Receiver, CurrentTime(), Auth}
-	body := MessageBody{Text}
+	header := MessageHeader{Type_, Command, UserName, Token}
+	body := MessageBody{ReceiverName, CurrentTime, Text}
 	message := Message{header, body}
 	mrsMsg, err := MarshalMessage(message)
 	if err != nil {
@@ -32,7 +30,6 @@ func TestMarshalMessage(t *testing.T) {
 }
 
 func TestUnmarshalMessage(t *testing.T) {
-	var ThisMessage Message
 	ThisMessage, err := UnmarshalMessage(byteMessage)
 	if err != nil {
 		t.Fatal("Error has occured: ", err)
@@ -66,22 +63,4 @@ func Test3UnmarshalRequest(t *testing.T) {
 	}
 	t.Logf("Message after unmarshaling has type %T \n", BodyInByte)
 	t.Logf("Message after unmarshaling : %+v \n ", BodyInByte)
-}
-
-func TestGetType(t *testing.T) {
-	Value, err := GetType(mappedRequest)
-	if err != nil || Value != "message" {
-		t.Fatal("Error has occured: ", err)
-	}
-	t.Logf("Type is %T \n", Value)
-	t.Logf("Type is %v", Value)
-}
-
-func TestGetCommand(t *testing.T) {
-	Command, err := GetCommand(mappedRequest)
-	if err != nil || Command != "sent message" {
-		t.Fatal("Error has occured: ", err)
-	}
-	t.Logf("Command is %T \n", Command)
-	t.Logf("Command is %v", Command)
 }

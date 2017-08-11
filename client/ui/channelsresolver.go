d5c03e88f0a6ae88b8f9f4083a6439ba1797081a
KitlerUA <volodymyr.kit.ua@gmail.com>
2017-08-11 21:39:23 +0300

    Improve channelsresolver

--- client/ui/channelsresolver.go
+++ client/ui/channelsresolver.go
@@ -17,9 +17,25 @@ func channelsResolver() {
 		case <-listener.QuitChannel:
 			return
 		case msg := <-listener.AuthorizationChannel:
-			receiveAuth(msg)
+			switch msg.Header.Command {
+			case config.GetConfig().MessageCommand.LoginIsSucc:
+				loginIsSuccessfully(msg)
+			case config.GetConfig().MessageCommand.LoginIsNotSucc:
+				loginIsNotSuccessfully(msg)
+			case config.GetConfig().MessageCommand.RegisterIsSucc:
+				registerIsSuccessfully(msg)
+			case config.GetConfig().MessageCommand.RegisterIsNotSucc:
+				registerIsNotSuccessfully(msg)
+			}
 		case msg := <-listener.MessageChannel:
-			receiveMessage(msg)
+			switch msg.Header.Command {
+			case config.GetConfig().MessageCommand.ReceiveMessage:
+				receiveNewMessage(msg)
+			case config.GetConfig().MessageCommand.MessageSent:
+				messageSent(msg)
+			case config.GetConfig().MessageCommand.MessageWasntSent:
+				messageWasntSent(msg)
+			}
 		case msg := <-listener.SettingsChannel:
 			receiveSettings(msg)
 		case <-listener.ContactsChannel:
@@ -27,108 +43,112 @@ func channelsResolver() {
 	}
 }
 
-func receiveAuth(message messageService.Message) {
-	switch message.Header.Command {
-	case config.GetConfig().MessageCommand.LoginIsSucc:
-		//login data is valid, so we open other channels to listen to server
-		listener.MessageChannel = make(chan messageService.Message)
-		listener.SettingsChannel = make(chan messageService.Message)
-		listener.ContactsChannel = make(chan messageService.Message)
-		//save received username and token
-		userinfo.CurrentUserInfo.UserName = message.Header.UserName
-		userinfo.CurrentUserInfo.UserName = message.Header.Token
-		//now we just inform UI that login was successfully
-		loger.Log.Infof("User %s login successfully.", message.Header.UserName)
-		qmlLogin.LoginDataIsValid(true)
-		qmlStatus.SendStatus("Loging successfully. Be happy and write messages :)")
-	case config.GetConfig().MessageCommand.LoginIsNotSucc:
-		//login data is invalid, so we close conn and inform UI
-		listener.QuitChannel <- struct{}{}
-		//we blocked Auth channel until user want to send login data again
-		listener.AuthorizationChannel = nil
-		//just close connection
-		err := connection.Close()
-		if err != nil {
-			loger.Log.Errorf("Cannot close connection after login is not successfully. %s", err)
-			//for client it doesn`t matter. connection can`t close == login is not successfully
-		}
-		loger.Log.Infof("User %s login wasn't successfully.", message.Header.UserName)
-		qmlLogin.LoginDataIsValid(false)
-		qmlStatus.SendStatus("Loging was unsuccessfully. Please check username or password again.")
-	case config.GetConfig().MessageCommand.RegisterIsSucc:
-		//however register successfully we need to close connection
-		//because all register actions have ended
-		listener.QuitChannel <- struct{}{}
-		//we blocked Auth channel until user want to send Auth data again
-		listener.AuthorizationChannel = nil
-		err := connection.Close()
-		if err != nil {
-			loger.Log.Errorf("Cannot close connection after register is successfully. %s", err)
-		}
-		loger.Log.Infof("User %s register successfully.", message.Header.UserName)
-		qmlRegister.RegisterDataIsValid(true)
-		qmlStatus.SendStatus("Register successfully. One more step and you`ll be happy!")
-	case config.GetConfig().MessageCommand.RegisterIsNotSucc:
-		//register is not successfully we need to close connection
-		listener.QuitChannel <- struct{}{}
-		//we blocked Auth channel until user want to send Auth data again
-		listener.AuthorizationChannel = nil
-		err := connection.Close()
-		if err != nil {
-			loger.Log.Errorf("Cannot close connection after register is not successfully. %s", err)
-			//for client it doesn`t matter. connection can`t close == register is not successfully
-		}
-		loger.Log.Infof("User %s register not successfully.", message.Header.UserName)
-		qmlRegister.RegisterDataIsValid(false)
-		qmlStatus.SendStatus("Register not successfully. Please, try again later!")
-	}
-}
-
-func receiveMessage(message messageService.Message) {
-	switch message.Header.Command {
-	case config.GetConfig().MessageCommand.ReceiveMessage:
-		sender := contacts.ContactsList.GetContactByUserName(message.Header.UserName)
-		if sender.IsBlocked {
-			loger.Log.Infof("Received message from blocked user - %s", sender.UserName)
-			return
-		}
-		var messageBody *messageService.MessageBody
-		err := json.Unmarshal(message.Body, &messageBody)
-		if err != nil {
-			loger.Log.Warningf("Cannot unmarshal received message. %s", err)
-			return
-		}
-		//TODO: change div-alignment property to display message properly
-		sender.MessageHistory += messageBody.Text
-		qmlContacts.SendLastMessage(messageBody.Text, contacts.ContactsList.IndexByUserName(sender.UserName))
-	case config.GetConfig().MessageCommand.MessageSent:
-		//we need to unmarshal body to get whom message was sent
-		var messageBody *messageService.MessageBody
-		err := json.Unmarshal(message.Body, &messageBody)
-		if err != nil {
-			loger.Log.Warningf("Cannot unmarshal received message. %s", err)
-			return
-		}
-		receiver := contacts.ContactsList.GetContactByUserName(messageBody.ReceiverName)
-		receiver.MessageHistory += messageBody.Text
-		qmlContacts.SendLastMessage(messageBody.Text, contacts.ContactsList.IndexByUserName(receiver.UserName))
-		qmlStatus.SendStatus("Message sent to " + messageBody.ReceiverName + " successfully")
-		qmlMessage.MessageSent(true)
-		loger.Log.Infof("Message was sent to %s .", messageBody.ReceiverName)
-	case config.GetConfig().MessageCommand.MessageWasntSent:
-		//we need to unmarshal body to get whom message was sent
-		var messageBody *messageService.MessageBody
-		err := json.Unmarshal(message.Body, &messageBody)
-		if err != nil {
-			loger.Log.Warningf("Cannot unmarshal received message. %s", err)
-			return
-		}
-		qmlStatus.SendStatus("Message wasn't sent to " + messageBody.ReceiverName)
-		qmlMessage.MessageSent(true)
-		loger.Log.Infof("Message wasn't sent to %s .", messageBody.ReceiverName)
-	}
-}
-
 func receiveSettings(message messageService.Message) {
 
+}
+
+func loginIsSuccessfully(message messageService.Message) {
+	//login data is valid, so we open other channels to listen to server
+	listener.MessageChannel = make(chan messageService.Message)
+	listener.SettingsChannel = make(chan messageService.Message)
+	listener.ContactsChannel = make(chan messageService.Message)
+	//save received username and token
+	userinfo.CurrentUserInfo.UserName = message.Header.UserName
+	userinfo.CurrentUserInfo.UserName = message.Header.Token
+	//now we just inform UI that login was successfully
+	loger.Log.Infof("User %s login successfully.", message.Header.UserName)
+	qmlLogin.LoginDataIsValid(true)
+	qmlStatus.SendStatus("Loging successfully. Be happy and write messages :)")
+}
+
+func loginIsNotSuccessfully(message messageService.Message) {
+	//login data is invalid, so we close conn and inform UI
+	listener.QuitChannel <- struct{}{}
+	//we blocked Auth channel until user want to send login data again
+	listener.AuthorizationChannel = nil
+	//just close connection
+	err := connection.Close()
+	if err != nil {
+		loger.Log.Errorf("Cannot close connection after login is not successfully. %s", err)
+		//for client it doesn`t matter. connection can`t close == login is not successfully
+	}
+	loger.Log.Infof("User %s login wasn't successfully.", message.Header.UserName)
+	qmlLogin.LoginDataIsValid(false)
+	qmlStatus.SendStatus("Loging was unsuccessfully. Please check username or password again.")
+}
+
+func registerIsSuccessfully(message messageService.Message) {
+	//however register successfully we need to close connection
+	//because all register actions have ended
+	listener.QuitChannel <- struct{}{}
+	//we blocked Auth channel until user want to send Auth data again
+	listener.AuthorizationChannel = nil
+	err := connection.Close()
+	if err != nil {
+		loger.Log.Errorf("Cannot close connection after register is successfully. %s", err)
+	}
+	loger.Log.Infof("User %s register successfully.", message.Header.UserName)
+	qmlRegister.RegisterDataIsValid(true)
+	qmlStatus.SendStatus("Register successfully. One more step and you`ll be happy!")
+}
+
+func registerIsNotSuccessfully(message messageService.Message) {
+	//register is not successfully we need to close connection
+	listener.QuitChannel <- struct{}{}
+	//we blocked Auth channel until user want to send Auth data again
+	listener.AuthorizationChannel = nil
+	err := connection.Close()
+	if err != nil {
+		loger.Log.Errorf("Cannot close connection after register is not successfully. %s", err)
+		//for client it doesn`t matter. connection can`t close == register is not successfully
+	}
+	loger.Log.Infof("User %s register not successfully.", message.Header.UserName)
+	qmlRegister.RegisterDataIsValid(false)
+	qmlStatus.SendStatus("Register not successfully. Please, try again later!")
+}
+
+func receiveNewMessage(message messageService.Message) {
+	sender := contacts.ContactsList.GetContactByUserName(message.Header.UserName)
+	if sender.IsBlocked {
+		loger.Log.Infof("Received message from blocked user - %s", sender.UserName)
+		return
+	}
+	var messageBody *messageService.MessageBody
+	err := json.Unmarshal(message.Body, &messageBody)
+	if err != nil {
+		loger.Log.Warningf("Cannot unmarshal received message. %s", err)
+		return
+	}
+	//TODO: change div-alignment property to display message properly
+	sender.MessageHistory += messageBody.Text
+	qmlContacts.SendLastMessage(messageBody.Text, contacts.ContactsList.IndexByUserName(sender.UserName))
+}
+
+func messageSent(message messageService.Message) {
+	//we need to unmarshal body to get whom message was sent
+	var messageBody *messageService.MessageBody
+	err := json.Unmarshal(message.Body, &messageBody)
+	if err != nil {
+		loger.Log.Warningf("Cannot unmarshal received message. %s", err)
+		return
+	}
+	receiver := contacts.ContactsList.GetContactByUserName(messageBody.ReceiverName)
+	receiver.MessageHistory += messageBody.Text
+	qmlContacts.SendLastMessage(messageBody.Text, contacts.ContactsList.IndexByUserName(receiver.UserName))
+	qmlStatus.SendStatus("Message sent to " + messageBody.ReceiverName + " successfully")
+	qmlMessage.MessageSent(true)
+	loger.Log.Infof("Message was sent to %s .", messageBody.ReceiverName)
+}
+
+func messageWasntSent(message messageService.Message) {
+	//we need to unmarshal body to get whom message was sent
+	var messageBody *messageService.MessageBody
+	err := json.Unmarshal(message.Body, &messageBody)
+	if err != nil {
+		loger.Log.Warningf("Cannot unmarshal received message. %s", err)
+		return
+	}
+	qmlStatus.SendStatus("Message wasn't sent to " + messageBody.ReceiverName)
+	qmlMessage.MessageSent(true)
+	loger.Log.Infof("Message wasn't sent to %s .", messageBody.ReceiverName)
 }

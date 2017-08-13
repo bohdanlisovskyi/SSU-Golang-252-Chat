57a59a7d896679db38501993ef3398412d6ac2fe
KitlerUA <volodymyr.kit.ua@gmail.com>
2017-08-13 17:05:35 +0300

    Some comments deleted

--- client/ui/settings.go
+++ client/ui/settings.go
@@ -22,13 +22,6 @@ func initQmlSettings(quickWidget *quick.QQuickWidget) {
 	//TODO: fix difference between c++ date and golang time
 	qmlSettings.ConnectApplySettings(func(oldPassword, newPassword, newNickname string, date time.Time, about string) {
 		//we need to form settings message and sent it to server
-		//code will be uncommented after fix Settings functionality
-		/*newMessageHeader := messageService.MessageHeader{
-			Type_:    config.GetConfig().MessageType.Settings,
-			Command:  config.GetConfig().MessageCommand.SendSettings,
-			UserName: userinfo.CurrentUserInfo.UserName,
-			Token:    userinfo.CurrentUserInfo.Token,
-		}*/
 
 		//after send message we just waiting for response
 

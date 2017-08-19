1748da6e8d88c49e98a1a0d3b85eef4000ecf25c
Bohdan <www.lbohdan@gmail.com>
2017-08-19 13:58:01 +0300

    add sh 5

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -6,3 +6,7 @@ cd .. && docker build -f Dockerfile -t $REPO:$COMMIT .
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install heroku-container-registry
+docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
